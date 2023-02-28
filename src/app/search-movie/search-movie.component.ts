9cda5ccded7bd7fe89193ee7c8d4c988a8ec72a7
serpentaire <glemoine@hotmail.fr>
2023-02-28 15:40:23 +0100

    nouvelle solution

--- src/app/search-movie/search-movie.component.ts
+++ src/app/search-movie/search-movie.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
-
-
+import { isRequiredValidator, rangeDateValidator }  from '../shared/validators'
 @Component({
   selector: 'app-search-movie',
   templateUrl: './search-movie.component.html',
@@ -10,72 +9,47 @@ import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors,
 
 
 export class SearchMovieComponent implements OnInit {
+  public formGroup: FormGroup;
   public inValidate = false;
-
- lesTypes = ["film", "série", "épisode"];
- lesFiches = ["complète", "courte"];
- typeInit = "série";
-
-  constructor(private fb: FormBuilder) { }
-
-    formGroup = this.fb.group({
-     title: new FormGroup({
-        identifiant: new FormControl('Hello Identifiant'),
-        titre: new FormControl('Hello titre'),
-      }, {validators: [isRequiredValidator('identifiant', 'titre')]}),
-      types: new FormControl(this.lesTypes, Validators.required),
-      anneesortie: new FormControl(1900, {validators: [rangeDateValidator(1920)]}),
-      fiches: new FormControl(this.lesFiches, Validators.required)
-    })
+  constructor(private _fb: FormBuilder) { }
+  lesTypes = [
+    {name : "film"},
+    {name : "série"},
+    {name : "épisode"}
+  ];
+  lesFiches = [
+    {name: "complète"},
+    {name: "courte"}
+  ]
+  date = new Date()
+  min= 1900
+  max = this.date.getFullYear()
 
   ngOnInit(): void {
-    this.formGroup.patchValue({
-      fiches: ["complète"]
+    this.formGroup = this._fb.group({
+      name: new FormGroup({
+        identifant: new FormControl(''),
+        titre: new FormControl('')
+      }, {validators: isRequiredValidator('titre','identifant')}),
+      types: new FormControl(this.lesTypes[1].name, Validators.required),
+      anneeDeSortie: new FormControl(null , {validators: rangeDateValidator(this.min,this.max)}),
+      fiches: new FormControl([])
     })
-  };
+    this.formGroup.patchValue({
+      fiches: this.lesFiches[0].name
+    })
 
-  filmSearch() {
-    console.log(JSON.stringify(this.formGroup.value));
-  };
-
+  }
   handleValidate() {
     this.inValidate = true
   }
-}
-interface ValidatorFn {
-  (control: AbstractControl<any, any>): ValidationErrors | null
-}
+  
+  onSubmit() {
+    console.log(JSON.stringify(this.formGroup.value));
+  }
 
-export function isRequiredValidator(identifiant: string, titre: string): ValidatorFn {
-  return (control: AbstractControl): ValidationErrors | null => {
-
-    const value1 = control.get(identifiant)?.value;
-
-    const value2 = control.get(titre)?.value;
-
-    if (value1 === "") {
-      return  Validators.required;
-    } else if (value2 === "") {
-      return  Validators.required
-    } else {
-      return null;
-    }
-  };
 }
 
-export function rangeDateValidator(anneesortie: number): ValidatorFn {
-  return (control: AbstractControl): ValidationErrors | null => {
-
-    const value1 = anneesortie
-    const date = new Date()
-
-    if (value1 > date.getFullYear() && value1 < 1900) {
-      return  Validators.required;
-    } else {
-      return null;
-    }
-  };
-}
 
 
 
---
+++ src/app/shared/validators.ts
@@ -0,0 +1,28 @@
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
+
+export function isRequiredValidator(titreControlName: string, identifiantControlName: string) {
+
+    return (control: AbstractControl): ValidationErrors | null => {
+      const titreControl = control.get(titreControlName);
+      const identifiantControl = control.get(identifiantControlName);
+
+      if (!titreControl?.value && !identifiantControl?.value) {
+        return { isRequired: true };
+      } else {
+        return null;
+      }
+
+    };
+  }
+
+  export function rangeDateValidator(min: number, max: number) {
+
+    return (control: AbstractControl): ValidationErrors | null => {
+      if ((isNaN(control.value) || control.value < min || control.value > max )){
+        return { min: { value: control.value, expected: 'between ' +min+ ' and '+ max  }}
+
+      }
+      return null;
+    }
+  }
